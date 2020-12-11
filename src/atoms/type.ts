6aca09ca0867d6f0fd5bdf32d9cfa15adcf99f89
bebetaro <takumi.kurokawa.dev@gmail.com>
2020-12-11 17:49:11 +0900

    feat(type): setup type

---
+++ src/atoms/type.ts
@@ -0,0 +1,12 @@
+export type Year = "90" | "00" | "10" | "None";
+export type Category =
+  | "None"
+  | "RPG"
+  | "Action"
+  | "Shooting"
+  | "Novel"
+  | "Mobile"
+  | "Sports"
+  | "MOBA"
+  | "Puzzle"
+  | "Other";
